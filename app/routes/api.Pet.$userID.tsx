8f8e53bf899076acfceb4cace5e6bcd8cc3cfbd3
kimsoeun07 <ksoeun6204@naver.com>
2023-10-31 23:08:21 +0900

    /

--- app/routes/api.Pet.$userID.tsx
+++ app/routes/api.Pet.$userID.tsx
@@ -8,8 +8,8 @@ export const loader:LoaderFunction = async ({params}) => {
         // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환)
         // const data = await collection.find({ "date": date.slice(0, -14) });
         await client.connect()
-        const data = await petcollection.find({ userID: userID })
-// .toArray();
+        const data = await petcollection.find({ userID: userID }).toArray();
+// 
         // 필요한 속성만 선택하여 응답
         const responseData = data.map((item) => ({
             imageURL: item.imageURL,
