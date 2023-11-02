773e0439d2e44d9c8578796e6eb23184e978b6b4
kimsoeun07 <ksoeun6204@naver.com>
2023-11-02 21:29:44 +0900

    /

--- app/routes/api.walkmemo.tsx
+++ app/routes/api.walkmemo.tsx
@@ -6,11 +6,12 @@ export const loader:LoaderFunction = async ({request}) => {
     const date = url.searchParams.get('date')
     const userID = url.searchParams.get('userID')
     if(!date || !userID) return json({ error: 'Server Error' }, 500)
+
     try {
 
-        // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환) { $regex: new RegExp(date) } userID
+        // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환) userID '2023-10-31' { $regex: new RegExp(date) }
         // const data = await collection.find({ "date": date.slice(0, -14) }); 
-        const data = await collection.find({  date: '2023-10-31', userID: 'ksoeun6204@naver.com'}).toArray();
+        const data = await collection.find({  date: '2023-10-31T11:37:03.670Z', userID: 'ksoeun6204@naver.com'}).toArray();
 
         // // 필요한 속성만 선택하여 응답
         const responseData = data.map((item) => ({
