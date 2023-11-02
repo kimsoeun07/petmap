5f311fc36a485b2d7d032aadc92c7b9b33c6588e
kimsoeun07 <ksoeun6204@naver.com>
2023-11-02 22:46:25 +0900

    /

--- app/routes/api.walkmemo.tsx
+++ app/routes/api.walkmemo.tsx
@@ -9,9 +9,9 @@ export const loader:LoaderFunction = async ({request}) => {
 
     try {
 
-        // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환) userID '2023-10-31' { $regex: new RegExp(date) }
+        // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환) '2023-10-31' 
         // const data = await collection.find({ "date": date.slice(0, -14) }); 
-        const data = await collection.find({}).toArray();
+        const data = await collection.find({ date: { $regex: new RegExp(date) }, userID: userID }).toArray();
 //   date: '2023-10-31T11:37:03.670Z', userID: 'ksoeun6204@naver.com'
         // // 필요한 속성만 선택하여 응답
         const responseData = data.map((item) => ({
