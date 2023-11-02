8db7ca6bbac80269eece9e4f6c8e0fee2bd3308c
kimsoeun07 <ksoeun6204@naver.com>
2023-11-02 23:17:12 +0900

    /

--- app/routes/api.walkmemo.tsx
+++ app/routes/api.walkmemo.tsx
@@ -4,14 +4,15 @@ import { collection } from "~/lib/mongodb";
 export const loader:LoaderFunction = async ({request}) => {
     const url = new URL(request.url)
     const date = url.searchParams.get('date')
-    const userID = url.searchParams.get('userID')
-    if(!date || !userID) return json({ error: 'Server Error' }, 500)
+    // const userID = url.searchParams.get('userID')
+    if(!date ) return json({ error: 'Server Error' }, 500)
+    // || !userID
 
     try {
 
         // 해당 날짜에 대한 데이터 조회 (예시로 모든 데이터 반환) '2023-10-31' 
-        // const data = await collection.find({ "date": date.slice(0, -14) }); 
-        const data = await collection.find({ date: { $regex: new RegExp(date) }, userID: userID }).toArray();
+        // const data = await collection.find({ "date": date.slice(0, -14) }); , userID: userID 
+        const data = await collection.find({ date: { $regex: new RegExp(date) }}).toArray();
 //   date: '2023-10-31T11:37:03.670Z', userID: 'ksoeun6204@naver.com'
         // // 필요한 속성만 선택하여 응답
         const responseData = data.map((item) => ({
