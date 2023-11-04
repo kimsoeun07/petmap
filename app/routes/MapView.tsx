a8717458f993f56f740547ddaddd9ffd3e6c3af3
kimsoeun07 <ksoeun6204@naver.com>
2023-11-04 12:17:07 +0900

    .

--- app/routes/MapView.tsx
+++ app/routes/MapView.tsx
@@ -11,6 +11,7 @@ const MapView = () => {
             // 문자열 형태의 메시지를 다시 객체나 배열 등의 원래 형태로 변환
             const data = JSON.parse(e.data);
             console.log(data);
+            console.log(typeof e.data)
             // const data = JSON.parse(JSON.stringify(e.data));
             setCoords(data);
 
